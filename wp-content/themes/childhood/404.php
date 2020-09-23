a797cc2e63a84ac259978231764eb1e12f5084b0
DenyskoD <deniskodima1@gmail.com>
2020-09-23 21:22:02 +0300

    seo + 404.php

---
+++ wp-content/themes/childhood/404.php
@@ -0,0 +1,8 @@
+<? get_header();?>
+
+<div class="container toys">
+    <h2 class="subtitle">
+        <a href="<? echo get_home_url(); ?>">Такой страницы не существует. Вернуться на главную</a>
+
+    </h2>
+</div>
