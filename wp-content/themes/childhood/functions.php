e6dd1c20b9aee5eea9b9dda05f5f8171f9bbb875
DenyskoD <deniskodima1@gmail.com>
2020-09-23 21:05:22 +0300

    pages for single posts

--- wp-content/themes/childhood/functions.php
+++ wp-content/themes/childhood/functions.php
@@ -16,7 +16,10 @@ function filter_nav_menu_link_attributes($atts, $item, $args) {
     if ($args->menu === 'Main') {
         $atts['class'] = 'header__nav-item';
 
-        if ($item=>current) {
+        if ($item->current) {
+            $atts['class'] .= ' header__nav-item-active';
+        }
+        if ($item->ID === 183 && (in_category('soft_toys') || in_category('edu_toys'))) {
             $atts['class'] .= ' header__nav-item-active';
         }
     };
